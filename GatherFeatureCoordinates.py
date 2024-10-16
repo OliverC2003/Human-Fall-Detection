1d2a22a70fb809b25755d17e00ec5962276d0713
OliverC2003 <olivercaushi@gmail.com>
2024-10-16 13:14:51 +0100

    2.2

--- GatherFeatureCoordinates.py
+++ GatherFeatureCoordinates.py
@@ -2,4 +2,4 @@ import pose_estimation
 
 path_to_video = r"C:\Users\olive\OneDrive\Documents\HFD\Data\Coffee_room_01\Coffee_room_01\Videos\video (1).avi"
 
-pose_estimation.store_features(path_to_video)
+pose_estimation.run_pose_estimation(path_to_video)
